0bb8a6cd7a770178e3985847f2c9f61031cad8b8
manlier <1490215053@qq.com>
2018-07-09 20:01:20 +0800

    stage work on 2018/07/09 20:00

---
+++ rnn/__init__.py
@@ -0,0 +1,106 @@
+import keras
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential, Model
+from keras import layers
+from keras.layers import recurrent
+from keras.callbacks import ModelCheckpoint
+from sklearn.model_selection import train_test_split
+import pandas as pd
+
+from utils import *
+
+base_dir = '../data'
+records_path = base_dir + '/records.csv'
+
+model_save_path = '../models/weights.{epoch:02d-{val_loss:.2f}}.hdf5'
+ck = keras.callbacks.ModelCheckpoint(model_save_path, monitor='loss', verbose=0)
+
+records = pd.read_csv(records_path)
+
+COMP_MAXLEN = 1
+USER_MAXLEN = 1
+EPOCHS = 6
+BATCH_SIZE = 32
+
+
+class WordTable:
+
+    def __init__(self, words):
+        self.words = sorted(set(words))
+        self.word_indices = {w: i + 1 for i, w in enumerate(self.words)}
+        self.indices_word = {i + 1: w for i, w in enumerate(self.words)}
+
+    def vocab_size(self):
+        return len(self.words) + 1
+
+
+def vectorize(data: iter
+              , comp_table: WordTable
+              , user_table: WordTable
+              , comp_maxlen: int
+              , user_maxlen: int):
+    uc = []
+    xc = []
+    yc = []
+
+    for user, comp, followComp in data:
+        u = [user_table.word_indices[user]]
+        x = [comp_table.word_indices[comp]]
+        y = np.zeros(len(comp_table.words) + 1)
+        y[comp_table.word_indices[followComp]] = 1
+        uc.append(u)
+        xc.append(x)
+        yc.append(y)
+
+    return pad_sequences(uc, maxlen=user_maxlen), pad_sequences(xc, maxlen=comp_maxlen), np.array(yc)
+
+
+vocab_comps = WordTable(pd.concat([records['compId'], records['followCompId']], ignore_index=True))
+vocab_users = WordTable(records['userId'])
+
+RNN = recurrent.LSTM
+EMBED_HIDDEN_SIZE = 50
+
+user = layers.Input(shape=(USER_MAXLEN,), dtype=np.float32)
+encoded_user = layers.Embedding(vocab_users.vocab_size(), EMBED_HIDDEN_SIZE)(user)
+encoded_user = layers.Dropout(0.3)(encoded_user)
+
+comp = layers.Input(shape=(COMP_MAXLEN,), dtype=np.float32)
+encoded_comp = layers.Embedding(vocab_comps.vocab_size(), EMBED_HIDDEN_SIZE)(comp)
+encoded_comp = layers.Dropout(0.3)(encoded_comp)
+encoded_comp = RNN(EMBED_HIDDEN_SIZE)(encoded_comp)
+encoded_comp = layers.RepeatVector(USER_MAXLEN)(encoded_comp)
+
+merged = layers.add([encoded_user, encoded_comp])
+merged = RNN(EMBED_HIDDEN_SIZE)(merged)
+merged = layers.Dropout(0.3)(merged)
+
+preds = layers.Dense(vocab_comps.vocab_size(), activation='softmax')(merged)
+
+model = Model([user, comp], preds)
+
+model.compile(optimizer='adam', loss=keras.losses.categorical_crossentropy
+              , metrics=['accuracy'])
+
+model.summary()
+
+
+def load_data(records):
+    for i, row in records.iterrows():
+        yield (row['userId'], row['compId'], row['followCompId'])
+
+
+train, test = train_test_split(list(load_data(records)), test_size=0.2)
+
+u, x, y = vectorize(train, vocab_comps, vocab_users, COMP_MAXLEN, USER_MAXLEN)
+tu, tx, ty = vectorize(test, vocab_comps, vocab_users, COMP_MAXLEN, USER_MAXLEN)
+
+print('Training...')
+model.fit([u, x], y,
+          epochs=EPOCHS,
+          validation_split=0.05, callbacks=[ck])
+
+loss, acc = model.evaluate([tu, tx], ty,
+                           batch_size=BATCH_SIZE)
+
+print('Test loss / test accuracy = {:.4f} / {:.4f}'.format(loss, acc))
---
+++ utils.py
@@ -0,0 +1,73 @@
+import pandas as pd
+import numpy as np
+import random
+
+
+
+
+def load_components(comp_path) -> pd.DataFrame:
+    """
+    载入构件
+    :param comp_path: 构件文件路径
+    :return:
+    """
+    df = pd.read_csv(comp_path, dtype={'compId': np.int32, 'name': str})
+    return df
+
+
+def generate_users(num_user) -> pd.DataFrame:
+    """
+    生成用户
+    :param num_user: 用户数量
+    :return:
+    """
+    df = pd.DataFrame(np.arange(0, num_user), index=np.arange(0, num_user), columns=['userId'])
+    return df
+
+
+def __generate_comp_pairs(comps: pd.DataFrame, num_pairs):
+    """
+    随机生成构件对
+    :param comps: 所有构件
+    :param num_pairs: 构件对的数量
+    :return:
+    """
+    for i in range(num_pairs):
+        comp_id_01 = random.choice(comps['compId'])
+        comp_id_02 = random.choice(comps['compId'])
+        yield (comp_id_01, comp_id_02)
+
+
+def random_generate_records(num_records: int, num_pairs: int, users: pd.DataFrame, comps: pd.DataFrame) -> pd.DataFrame:
+    """
+    随机生成构件使用纪录
+    :param num_records: 纪录条数
+    :param num_pairs: 构件对的数量
+    :param users: 用户
+    :param comps: 构件
+    :return:
+    """
+    comp_pairs = list(__generate_comp_pairs(comps, num_pairs))
+
+    records = []
+
+    for i in range(num_records):
+        comp_pair = random.choice(comp_pairs)
+        user_id = random.choice(users.index)
+        records.append((user_id, comp_pair[0], comp_pair[1]))
+
+    return pd.DataFrame(records, columns=['userId', 'compId', 'followCompId'])
+
+
+def decode_records(records: pd.DataFrame, comps: pd.DataFrame) -> pd.DataFrame:
+    """
+    将纪录中的构件Id替换为构件名
+    :param records:
+    :param comps:
+    :return:
+    """
+    stage = pd.merge(records, comps, on='compId')
+    stage = pd.merge(stage, comps, left_on='followCompId', right_on='compId', how='left') \
+        .drop(columns=['compId_y', 'compId_x', 'followCompId']) \
+        .rename(columns={'name_x': 'comp_name', 'name_y': 'follow_comp_name'})
+    return stage
